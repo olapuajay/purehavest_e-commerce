017707a42b3b2b403cb1b594baec7946521f7c9d
abhinavbestha26 <abhinavbestha@gmail.com>
2025-07-05 13:09:21 +0530

    Created login and registered Api's

--- Server/server.js
+++ Server/server.js
@@ -1,14 +1,19 @@
-import express from 'express'
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
 const app = express();
 
+app.use(express.json);
+
+app.use("/api/auth", authRoutes);
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from server!")
-})
+  res.send("Hello from server!");
+});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
