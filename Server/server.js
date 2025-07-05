4ada16c4dd655ee3358524a43ed53be73688677e
Olapu Ajay <olapuajay@gmail.com>
2025-07-05 11:01:17 +0530

    created tempData folder in server

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!")
+})
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
