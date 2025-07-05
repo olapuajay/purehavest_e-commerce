b5151c7795a3fccd4574bb404d8e9d22ebc23b2d
Olapu Ajay <olapuajay@gmail.com>
2025-07-05 11:18:22 +0530

    created users file

---
+++ Server/tempData/users.js
@@ -0,0 +1,25 @@
+let users = [
+  {
+    id: "u1",
+    name: "ajay",
+    email: "ajay@gmail.com",
+    password: "1234",
+    role: "admin"
+  },
+  {
+    id: "u2",
+    name: "abhinav",
+    email: "abhinav@gmail.com",
+    password: "1234",
+    role: "user"
+  },
+  {
+    id: "u3",
+    name: "avinash",
+    email: "avinash@gmail.com",
+    password: "1234",
+    role: "farmer"
+  },
+]
+
+module.exports = users;
