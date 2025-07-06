5e6c272c03cad50c84f7d7691dd449b750c65ef2
Olapu Ajay <olapuajay@gmail.com>
2025-07-06 11:09:14 +0530

    updated folder structure

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from './routes/authRoutes.js';
 dotenv.config();
 
 const app = express();
 
 app.use(express.json);
 
-app.use("/api/auth", authRoutes);
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8001;
 
 app.get("/", (req, res) => {
   res.send("Hello from server!");
