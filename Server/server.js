adc220c941959cb11446a0ed5e2aeacdca429e58
Olapu Ajay <olapuajay@gmail.com>
2025-07-08 22:05:58 +0530

    implemented few farmers routes and APIs

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,7 @@ dotenv.config();
 import express from "express";
 import connectDB from "./config/db.js";
 import userRoutes from './routes/userRoutes.js';
+import farmerRoutes from './routes/farmerRoutes.js'
 
 
 connectDB();
@@ -12,6 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/users", userRoutes);
+app.use("/api/farmers", farmerRoutes);
 
 const port = process.env.PORT || 8001;
 
