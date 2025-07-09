e4e7a2d99e6714a67dcec3c81b63409d33d27216
abhinavbestha26 <abhinavbestha@gmail.com>
2025-07-09 21:08:26 +0530

    added controllers to the admin to access the users

--- Server/controllers/adminController.js
+++ Server/controllers/adminController.js
@@ -48,3 +48,40 @@ export const deleteProductByAdmin = async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 };
+
+
+
+// Get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json({ message: "Users List", users });
+    
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+// Get a user by ID
+export const getUserById = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({ message: "User found", user });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+// Delete a user by ID
+export const deleteUserById = async (req, res) => {
+  try {
+    const deletedUser = await userModel.findByIdAndDelete(req.params.id);
+    if (!deletedUser) return res.status(404).json({ message: "User not found" });
+
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
