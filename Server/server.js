567e96b1c64dc2cc076e9f039f74c74c1265c66a
Olapu Ajay <olapuajay@gmail.com>
2025-07-09 21:32:47 +0530

    :wq Revert "Merge pull request #17 from abhinavbestha26/feature/admin-test" :wq
    This reverts commit 81de80aaca3d834bd3c60a9afae460b43e4c8dd8, reversing
    changes made to e59cac12faa821bcce15bec2be850972f895aee6.
    
    :wq

--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,19 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import userRoutes from './routes/userRoutes.js';
-import farmerRoutes from './routes/farmerRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-import adminRoutes from './routes/adminRoutes.js';
-
+dotenv.config();
 
 connectDB();
 
 const app = express();
-app.use(express.json());
 
-app.use("/api/users", userRoutes);
-app.use("/api/farmers", farmerRoutes);
-app.use("/api/farmers/products", productRoutes);
-app.use("/api/admin", adminRoutes);
+app.use(express.json);
 
 const port = process.env.PORT || 8001;
+
+app.get("/", (req, res) => {
+  res.send("Hello from server!");
+});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
