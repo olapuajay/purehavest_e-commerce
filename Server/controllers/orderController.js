1beca3ea6a697f9adbe27e6f1fcc60c5e110dc0d
Olapu Ajay <olapuajay@gmail.com>
2025-07-10 21:05:29 +0530

    Implemented feature: admin changes the status of placed order(shipped, delivred)

--- Server/controllers/orderController.js
+++ Server/controllers/orderController.js
@@ -55,3 +55,24 @@ export const getAllOrders = async (req, res) => {
     res.status(500).json({ message: "Failed to fetch all orders" });
   }
 };
+
+export const updateOrderStatus = async (req, res) => {
+  try {
+    const { status } = req.body;
+    const validStatus = ["confirmed", "shipped", "delivered"];
+    if(!validStatus.includes(status)) {
+      return res.status(400).json({ message: "Invalid status value" });
+    }
+    const order = await orderModel.findById(req.params.id);
+    if(!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+    order.status = status;
+    const updatedOrder = await order.save();
+    
+    res.status(200).json({ message: "Order status updated", order: updatedOrder, });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Failed to update order status" });
+  }
+};
--- Server/routes/orderRoutes.js
+++ Server/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authenticate, authorize } from "../middleware/authMiddleware.js";
-import { placeOrder, getUserOrders, getFarmerOrders, getAllOrders } from "../controllers/orderController.js";
+import { placeOrder, getUserOrders, getFarmerOrders, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
 
 const Router = express.Router();
 
@@ -9,5 +9,7 @@ Router.get("/myorders", authenticate, getUserOrders);
 
 Router.get("/farmer", authenticate, authorize("farmer"), getFarmerOrders);
 Router.get("/", authenticate, authorize("admin"), getAllOrders);
+
+Router.patch("/:id/status", authenticate, authorize("admin"), updateOrderStatus);
 
 export default Router;
