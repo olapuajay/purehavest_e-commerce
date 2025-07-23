93906ceb31b0a021791fdc911cb3f3eb8fb7745a
Olapu Ajay <olapuajay@gmail.com>
2025-07-23 15:31:48 +0530

    updated logos for PWA

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -17,17 +17,17 @@ export default defineConfig({
         theme_color: "#ffffff",
         icons: [
           {
-            src: "/icons/sun-192x192.png",
+            src: "/icons/logo-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/sun-512x512.png",
+            src: "/icons/logo-512x512.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "/icons/sun-512x512.png",
+            src: "/icons/logo-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
