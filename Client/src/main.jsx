f536937782e9458b6c1e6b4ef007e75b8cf8648a
Olapu Ajay <olapuajay@gmail.com>
2025-07-23 14:21:38 +0530

    implemented PWA

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -7,6 +7,19 @@ import { AuthProvider } from './components/contexts/AuthContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { CartProvider } from './components/contexts/cartContext.jsx'
 
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline.");
+  }
+});
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -2,9 +2,40 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Pure Harvest",
+        short_name: "Pure Harvest",
+        description: "MERN Stack",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/sun-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/sun-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/sun-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 3000
   },
